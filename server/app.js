7bfc37c125d12154c2061d4b7dd259944cc3ea59
kimts2627 <kimtstststs@gmail.com>
2021-07-22 00:23:17 +0900

    :sparkles: NEW: create server
    create server with express.js and request cats form google

--- server/app.js
+++ server/app.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { googleCatHunter } = require('./bin/crawler');
 
 const app = express();
 
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
 	res.send('Hello Cats!');
 });
 
-app.get('/cats', (req, res) => {
-	res.send('cats');
+app.get('/cats', async (req, res) => {
+	const cats = await googleCatHunter();
+	console.log(cats);
+	res.send(cats);
 });
 
 app.listen(port, () => {
--- server/bin/crawler.js
+++ server/bin/crawler.js
@@ -1,2 +1,16 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
+
+const google = 'https://google.com';
+
+const requestCat = async (requestUrl) => {
+	const res = await axios.get(requestUrl);
+	return res;
+};
+
+exports.googleCatHunter = async () => {
+	const keyword = encodeURIComponent('cat meme');
+	let url = `${google}/search?q=${keyword}&tbm=isch&tbs=qdr:y`;
+	const res = await requestCat(url);
+	return res.data;
+};
